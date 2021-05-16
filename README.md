2a0c797a164d94922166cf79d16c14c8239b6c64
Guillaume Algis <guillaume.algis@gmail.com>
2021-05-16 17:51:53 +0200

    Script now reads .swiftlint.yml to prevent explicitely disabled rules to be proposed

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 A script to discover and list opt-in rules you have not yet enabled in your [SwiftLint](https://github.com/realm/SwiftLint) config.
 
 # How to use
+
 ## Setup
 
 You need Python 3 installed on your system. The easiest way is probably to install it using [homebrew](https://brew.sh/):
@@ -11,15 +12,40 @@ You need Python 3 installed on your system. The easiest way is probably to insta
 brew install python3
 ```
 
-but if you want to keep things clean an organized (and if you're using multiple python versions in parallel, I'd recommend looking at [pyenv](https://github.com/pyenv/pyenv)).
+but if you want to keep things clean an organised (and if you're using multiple python versions in parallel, I'd recommend looking at [pyenv](https://github.com/pyenv/pyenv)).
 
-## Running the script
+## Usage
 
-Move to your project directory (where the `.swiftlint.yml` file is), then run:
+Clone this repository, and run the script by passing your Swift project directory (where the `.swiftlint.yml` file is) as argument:
 
 ```shell
-python3 swiftlint_discover.py
+./swiftlint_discover.py /path/to/your/swift/project
 ```
 
+### Only list new SwiftLint rules
 
+If you run this script regularly, you'll notice that it will output the same opt-in rules that you don't want to enable again and again. To prevent that, you can explicitly disable these rules in your `.swiftlint.yml`, and the script will be smart enough to not propose these rules ever again.
 
+For the script to be able to read your SwiftLint configuration though, you have to install the PyYAML dependency. You can either install it globally or in a virtualenv (recommended):
+
+```shell
+python3 -m venv $(pwd)/venv
+. ./venv/bin/activate
+pip install -r requirements.txt # This will install PyYAML
+```
+
+For example, if you don't want to ever enable the opt-in rule `explicit_acl`, you can add this to your `.swiftlint.yml`, and it will not be listed as a possible rule to enable by the script:
+
+```yaml
+disabled_rules:
+  # The following are opt-in rules that we don't want to enable, but that we're
+  # keeping here to be able to know which rules are new to SwiftLint when it
+  # is updated:
+  - explicit_acl
+```
+
+(I personally like to keep the comment in the example to remember why I have disabled opt-in rules in my config, which does nothing from a SwiftLint point of view).
+
+# Contact
+
+Guillaume Algis ([@guillaumealgis](https://twitter.com/guillaumealgis))
--- swiftlint_discover.py
+++ swiftlint_discover.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import argparse
+import os
 import subprocess
 from subprocess import PIPE, DEVNULL
 
@@ -10,17 +12,28 @@ class ParserError(Exception):
         super().__init__(message)
 
 
+class InvalidDirectoryError(Exception):
+    pass
+
+
 class Rule:
     # pylint: disable=too-many-arguments
     def __init__(
-        self, identifier, opt_in, correctable, enabled, kind, analyzer, configuration
+        self,
+        identifier,
+        opt_in=False,
+        correctable=False,
+        enabled=False,
+        kind="",
+        analyzer=False,
+        configuration="",
     ):
         self.identifier = identifier.strip()
-        self.opt_in = bool_from_string(opt_in.strip())
-        self.correctable = bool_from_string(correctable.strip())
-        self.enabled = bool_from_string(enabled.strip())
+        self.opt_in = bool_from_string(opt_in)
+        self.correctable = bool_from_string(correctable)
+        self.enabled = bool_from_string(enabled)
         self.kind = kind.strip()
-        self.analyzer = bool_from_string(analyzer.strip())
+        self.analyzer = bool_from_string(analyzer)
         self.configuration = configuration.strip()
 
     def pretty_print(self, ident=2):
@@ -30,15 +43,57 @@ class Rule:
         print(f"{s}  https://realm.github.io/SwiftLint/{self.identifier}.html")
         print()
 
+    def __eq__(self, other):
+        if isinstance(other, Rule):
+            return self.identifier == other.identifier
+        return False
+
+    def __hash__(self):
+        return hash(self.identifier)
+
+    def __repr__(self):
+        str_repr = f'Rule<"{self.identifier}"'
+        if self.enabled:
+            str_repr += " enabled"
+        else:
+            str_repr += " disabled"
+        if self.opt_in:
+            str_repr += " opt_in"
+        if self.analyzer:
+            str_repr += " analyzer"
+        str_repr += ">"
+        return str_repr
+
 
 def bool_from_string(bool_str):
-    lower_bool_str = bool_str.lower()
-    if lower_bool_str == "yes":
+    if isinstance(bool_str, bool):
+        return bool_str
+
+    clean_str = bool_str.strip().lower()
+    if clean_str == "yes":
         return True
-    if lower_bool_str == "no":
+    if clean_str == "no":
         return False
 
     raise ValueError(f"Unexpected boolean string '{bool_str}'")
+
+
+def print_summary(new_rules):
+    disabled_opt_in_rules = []
+    disabled_analyzer_rules = []
+    for rule in new_rules:
+        if rule.enabled:
+            continue
+        if rule.analyzer:
+            disabled_analyzer_rules.append(rule)
+        elif rule.opt_in:
+            disabled_opt_in_rules.append(rule)
+
+    if disabled_opt_in_rules or disabled_analyzer_rules:
+        print_section("opt-in", disabled_opt_in_rules)
+        print_section("analyzer", disabled_analyzer_rules)
+    else:
+        print("No new rules to enable!")
 
 
 def print_section(rules_type, rules):
@@ -51,10 +106,14 @@ def print_section(rules_type, rules):
         rule.pretty_print()
 
 
-def main():
-    # FIXXME
+def collect_disabled_rules(project_dir):
     process = subprocess.run(
-        ["swiftlint", "rules"], stdout=PIPE, stderr=DEVNULL, universal_newlines=True
+        ["swiftlint", "rules"],
+        stdout=PIPE,
+        stderr=DEVNULL,
+        universal_newlines=True,
+        check=True,
+        cwd=project_dir,
     )
 
     rules = []
@@ -71,21 +130,76 @@ def main():
         rule = Rule(*columns)
         rules.append(rule)
 
-    disabled_opt_in_rules = []
-    disabled_analyzer_rules = []
-    for rule in rules:
-        if rule.enabled:
-            continue
-        if rule.analyzer:
-            disabled_analyzer_rules.append(rule)
-        elif rule.opt_in:
-            disabled_opt_in_rules.append(rule)
+    return {rule for rule in rules if not rule.enabled}
 
-    if disabled_opt_in_rules or disabled_analyzer_rules:
-        print_section("opt-in", disabled_opt_in_rules)
-        print_section("analyzer", disabled_analyzer_rules)
-    else:
-        print("No new rules to enable!")
+
+def load_swiftlint_conf(project_dir):
+    # pylint: disable=import-outside-toplevel
+
+    try:
+        import yaml
+    except ModuleNotFoundError:
+        print(
+            "PyYAML not found. Not checking disabled rules in your SwiftLint configuration file."
+        )
+        return None
+
+    # Try to use the LibYAML C bindings if available.
+    try:
+        from yaml import CLoader as Loader
+    except ImportError:
+        from yaml import Loader
+
+    conf_path = os.path.join(project_dir, ".swiftlint.yml")
+    try:
+        with open(conf_path, "r") as stream:
+            conf = yaml.load(stream, Loader=Loader)
+            return conf
+    except FileNotFoundError:
+        return None
+
+
+def collect_explicitly_disabled_rules(project_dir):
+    conf = load_swiftlint_conf(project_dir)
+
+    try:
+        disabled_rules = conf["disabled_rules"]
+    except KeyError:
+        return set()
+
+    return {Rule(identifier) for identifier in disabled_rules}
+
+
+def parse_args():
+    def dir_path(string):
+        if not os.path.isdir(string):
+            raise InvalidDirectoryError(string)
+        return string
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "dir",
+        type=dir_path,
+        help="path to the project directory containing the .swiftlint configuration file. Defaults to the current working directory if omited",
+        default=os.getcwd(),
+        nargs="?",
+    )
+
+    args = parser.parse_args()
+    args.dir = os.path.abspath(args.dir)
+
+    return args
+
+
+def main():
+
+    args = parse_args()
+
+    disabled_rules = collect_disabled_rules(args.dir)
+    explicitly_disabled_rules = collect_explicitly_disabled_rules(args.dir)
+    new_rules = disabled_rules - explicitly_disabled_rules
+
+    print_summary(new_rules)
 
 
 if __name__ == "__main__":
